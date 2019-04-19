c7f88d9697011469242ad9cc071785ae1f1371ce
Welliton Paiva <wellitonpaiva@gmail.com>
2019-04-18 22:51:33 -0300

    creating first context as /

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,3 +1,8 @@
-public class HelloWorld {
+import io.javalin.Javalin;
 
+public class HelloWorld {
+    public static void main(String[] args) {
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
+    }
 }
